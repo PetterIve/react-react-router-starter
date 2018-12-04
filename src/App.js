2c6045bfe3fee8a8bd4692903c121e14958b086d
Petter Iversen <iversen332@gmail.com>
2018-12-04 22:54:59 +0100

    refactor components -> component-library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from "./components/button/button";
+import {Button} from "./component-library/button/button";
 
 class App extends Component {
   render() {
