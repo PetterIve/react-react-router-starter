37088e8634d008b66f6879bbc34e5f2c0febb7cb
Petter Iversen <iversen332@gmail.com>
2018-12-04 23:08:13 +0100

    add home link

--- src/routes/component-example/component-example.js
+++ src/routes/component-example/component-example.js
@@ -2,11 +2,14 @@ import * as React from "react";
 import {Col} from "../../component-library/col/col";
 import {Row} from "../../component-library/row/row";
 import {Button} from "../../component-library/button/button";
+import {RouteMap} from "../routeMap";
+import {Link} from "react-router-dom";
 
 export class ComponentExample extends React.Component {
     render() {
         return (
             <Col>
+                <Link to={RouteMap.home}>Home</Link>
                 <Row>
                     <Col>
                         Not rounded
