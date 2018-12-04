6b891e875fe16c1284b88201cb619d78f9cae12b
Petter Iversen <iversen332@gmail.com>
2018-12-04 22:51:20 +0100

    add button component

---
+++ src/components/button/button.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as classnames from 'classnames';
+
+import './button.css';
+
+export const Button = (props) => {
+    const className = classnames (
+        'button',
+        { rounded: props.rounded }
+    );
+    return (
+        <button
+            className={className}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    )
+}
